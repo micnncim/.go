e4c28d8850229f619c66c79d50a957f9179fa77f
micnncim <micnncim@gmail.com>
2020-02-10 19:10:07 +0900

    Update make

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -1,3 +1,6 @@
+# ----------------------------------------------------------------------------
+# defines
+
 define _conditional_include
 	$(if $(filter $(1),$(MAKEFILE_LIST)),,$(eval include $(1)))
 endef
