ded0218124ffab7ff238da04e2fbf34e7aa844e8
micnncim <micnncim@gmail.com>
2020-02-29 20:43:38 +0900

    Update make

--- make/docker.mk
+++ make/docker.mk
@@ -11,17 +11,7 @@ DOCKER_BUILD_TAG ?= latest
 
 .PHONY: docker/build
 docker/build:  ## Build docker image.
-ifeq ($(DOCKER_BUILDKIT),1)
-	buildctl build \
-		--frontend dockerfile.v0 \
-		--local context=. \
-		--local dockerfile=. \
-		--output type=image,name=$(DOCKER_REGISTRY)/$(APP):${DOCKER_BUILD_TAG}
-else
-	docker image build . \
-		${DOCKER_BUILD_ARGS} \
-		-t $(DOCKER_REGISTRY)/$(APP):${DOCKER_BUILD_TAG}
-endif
+	docker image build . ${DOCKER_BUILD_ARGS} -t $(DOCKER_REGISTRY)/$(APP):${DOCKER_BUILD_TAG}
 
 .PHONY: docker/push
 docker/push:  ## Push docker image to docker registry.
